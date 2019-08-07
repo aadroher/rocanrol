a840dbd7a40eaafdb5e5b3f4d41267cb5df3137e
Armand Adroher <aadroher@gmail.com>
2019-08-08 00:46:26 +0100

    Removes the coverage threshold.

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,3 @@
 module.exports = {
   collectCoverageFrom: ['src/**/*.js'],
-  coverageThreshold: {
-    global: {
-      branches: 90,
-      functions: 90,
-      lines: 90,
-      statements: 90,
-    },
-  },
 };
